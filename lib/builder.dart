514a648130cac6a1c167a19a2e68303715aa0ea6
Tushar Nikam <champ96k@gmail.com>
2025-05-24 01:27:19 +0530

    github action test

--- lib/builder.dart
+++ lib/builder.dart
@@ -3,5 +3,9 @@ import 'package:source_gen/source_gen.dart';
 
 import 'src/generator/quanta_generator.dart';
 
+/// Creates a builder that generates code using the QuantaGenerator.
+///
+/// This builder is used to generate `.g.dart` files containing the generated code
+/// from the QuantaGenerator.
 Builder quantaBuilder(BuilderOptions options) =>
     PartBuilder([QuantaGenerator()], '.g.dart');
